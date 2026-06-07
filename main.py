e68080c944f3a5ff6e02953b9c8deb1252ae2e6c
anonimo-own <anonimo070_me@proton.me>
2026-06-07 00:32:38 -0300

    add comentarios e tambem aprimorado as mensagens do print, de resto o codigo continua o mesmo

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-n1 = float(input('n1: '))
-n2 = float(input('n2: '))
+#Tela inicial
+print('''
+Ola
+      Bem vindo ao calc do anonimo feito em python
+      so poderemos fazer contas de apenas 2 numeros (tanto int e float) e apenas soma e subtair
+      irei adicionar mais coisas depois
+      ''')
 
-print('''escolha qual operaçao voce vai usar
-soma = 1
-subtaçao = 2''')
+#Definir os valores da var dos numeros
+n1 = float(input('Primeiro numero: '))
+n2 = float(input('Segundo numero: '))
+
+#Explicar como que escolher entre soma e subtaçao
+print('''voce poderar escolher entre soma e subtaçao sendo eles:
+
+      soma: 1
+      subtaçao: 2
+
+      caso coloque um valor errado iriar da erro e o calc vai fecha''')
+
+#Valor da var operaçao e tambem a parte das contas
 operacao = int(input('operaçao: '))
 
 if operacao == 1:
@@ -13,5 +28,7 @@ elif operacao == 2:
     print (n1, '-', n2, '=', n1 - n2)
 
 else:
-    print ('erro')
+    print ('''
+    ERROR, voce colocou um valor que nao tava disponivel, porfavo inicie o programa novamente
+    ''')
 
